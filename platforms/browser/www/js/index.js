492e758391ec7561d481cc29d9a22311399549ee
jpenmart <jaime.pena.martin.sa@everis.com>
2019-06-06 17:03:23 +0200

    ADD persistance with localStorage

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,7 +1,20 @@
-function addElement() {
+var textList = [];
+
+function addElement(textToCreate) {
     var list = document.getElementById("list");
-    var text = document.getElementById("taskToAdd").value;
+    if (textToCreate) {
+        var text = textToCreate;
+        console.log(text)
+    } else {
+        var text = document.getElementById("taskToAdd").value;
+        console.log(text)
+        textList.push(text);
+    }
+    
+    
+    localStorage.setItem("textList", JSON.stringify(textList));
     var li = document.createElement("li");
+    li.setAttribute("text", text);
     var buttonToDeleteElement = document.createElement("button");
     buttonToDeleteElement.setAttribute("type", "button");
     buttonToDeleteElement.setAttribute("onclick", "deleteElementFromList(this)");
@@ -12,5 +25,22 @@ function addElement() {
 }
 
 function deleteElementFromList(button) {
+    textList = textList.filter(function(value, index, arr){
+        return button.parentNode.getAttribute('text') !== value;
+    });
+    console.log(textList);
+    localStorage.setItem("textList", JSON.stringify(textList));
     document.getElementById("list").removeChild(button.parentNode);
+}
+
+window.onload = function chargeList() {
+    textList = JSON.parse(localStorage.getItem("textList"));
+    if (textList){
+        for (let txt of textList) {
+            addElement(txt);
+        }
+    } else {
+        textList = [];
+    }
+
 }
